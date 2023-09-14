11d7fe615da4393396ef48225d371bb651ce0df1
JulianDominicSchreib <julian.schreiber@nl-it-consulting.de>
2023-09-14 11:50:17 +0200

    eigenes Testprotocol gebaut, Serverarchitektur verbessert

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import socket
+import threading
 
 def start_server():
     # Create a TCP socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
     # Bind the socket to a specific IP address and port
     server_address = ('localhost', 9999)
     server_socket.bind(server_address)
 
     # Listen for incoming connections
-    server_socket.listen(1)
+    server_socket.listen(5)
     print('Server started. Listening for incoming connections...')
 
     while True:
@@ -17,16 +18,19 @@ def start_server():
         client_socket, client_address = server_socket.accept()
         print(f'Incoming connection from: {client_address}')
 
-        # Receive and process data from the client
-        data = client_socket.recv(1024).decode()
-        print(f'Received data from client: {data}')
+        client_handler = threading.Thread(target = client_Handler, args=(client_socket,))
+        client_handler.start()
 
-        # Send a response back to the client
-        response = 'Server received the message successfully'
-        client_socket.send(response.encode())
 
-        # Close the client connection
-        client_socket.close()
+def client_Handler(client_socket):
+    # send a message to the client
+    client_socket.send("ready".encode())
+    # receive and display a message from the client
+    request = client_socket.recv(1024).decode()
+    print("Received \"" + request + "\" from client")
+    # close the connection again
+    client_socket.close()
+    print("Connection closed")
 
 if __name__ == '__main__':
     start_server()
